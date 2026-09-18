429f83077a63b5687701c5a5bf0e585e7461353c
Ryan Peters <ryanpeters@microsoft.com>
2026-09-18 11:12:13 -0400

    add acr and workload identiy

--- main.tf
+++ main.tf
@@ -42,8 +42,166 @@ resource "azurerm_subnet" "bastion" {
   address_prefixes     = ["10.0.5.0/26"]
 }
 
+resource "azurerm_subnet" "privateEndpoints" {
+  name                 = "snet-private-endpoints"
+  resource_group_name  = azurerm_resource_group.aks.name
+  virtual_network_name = azurerm_virtual_network.aks.name
+  address_prefixes     = ["10.0.5.64/27"]
+}
+
+resource "azurerm_network_security_group" "nodes" {
+  name                = "nsg-aks-nodes"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  tags                = var.tags
+}
+
+resource "azurerm_network_security_group" "apiServer" {
+  name                = "nsg-aks-api-server"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  tags                = var.tags
+}
+
+resource "azurerm_network_security_group" "privateEndpoints" {
+  name                = "nsg-private-endpoints"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  tags                = var.tags
+}
+
+resource "azurerm_network_security_group" "bastion" {
+  name                = "nsg-bastion"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  tags                = var.tags
+
+  security_rule {
+    name                       = "AllowHttpsInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowGatewayManagerInbound"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "GatewayManager"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowAzureLoadBalancerInbound"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "AzureLoadBalancer"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowBastionHostCommunicationInbound"
+    priority                   = 130
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["8080", "5701"]
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "VirtualNetwork"
+  }
+
+  security_rule {
+    name                       = "AllowSshRdpOutbound"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["22", "3389"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "VirtualNetwork"
+  }
+
+  security_rule {
+    name                       = "AllowAzureCloudOutbound"
+    priority                   = 110
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "AzureCloud"
+  }
+
+  security_rule {
+    name                       = "AllowBastionCommunicationOutbound"
+    priority                   = 120
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["8080", "5701"]
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "VirtualNetwork"
+  }
+
+  security_rule {
+    name                       = "AllowHttpOutbound"
+    priority                   = 130
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "Internet"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nodes" {
+  subnet_id                 = azurerm_subnet.nodes.id
+  network_security_group_id = azurerm_network_security_group.nodes.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "apiServer" {
+  subnet_id                 = azurerm_subnet.apiServer.id
+  network_security_group_id = azurerm_network_security_group.apiServer.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "bastion" {
+  subnet_id                 = azurerm_subnet.bastion.id
+  network_security_group_id = azurerm_network_security_group.bastion.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "privateEndpoints" {
+  subnet_id                 = azurerm_subnet.privateEndpoints.id
+  network_security_group_id = azurerm_network_security_group.privateEndpoints.id
+}
+
 resource "azurerm_user_assigned_identity" "aks" {
   name                = "id-${var.cluster_name}"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  tags                = var.tags
+}
+
+resource "azurerm_user_assigned_identity" "workload" {
+  name                = "id-workload-${var.cluster_name}"
   location            = azurerm_resource_group.aks.location
   resource_group_name = azurerm_resource_group.aks.name
   tags                = var.tags
@@ -54,6 +212,51 @@ resource "azurerm_role_assignment" "aksNetworkContributor" {
   role_definition_name = "Network Contributor"
   principal_id         = azurerm_user_assigned_identity.aks.principal_id
   principal_type       = "ServicePrincipal"
+}
+
+resource "azurerm_container_registry" "aks" {
+  name                          = "acr${var.cluster_name}"
+  resource_group_name           = azurerm_resource_group.aks.name
+  location                      = azurerm_resource_group.aks.location
+  sku                           = "Premium"
+  admin_enabled                 = false
+  public_network_access_enabled = false
+  tags                          = var.tags
+}
+
+resource "azurerm_private_dns_zone" "acr" {
+  name                = "privatelink.azurecr.io"
+  resource_group_name = azurerm_resource_group.aks.name
+  tags                = var.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "acr" {
+  name                  = "link-acr-${var.cluster_name}"
+  resource_group_name   = azurerm_resource_group.aks.name
+  private_dns_zone_name = azurerm_private_dns_zone.acr.name
+  virtual_network_id    = azurerm_virtual_network.aks.id
+  registration_enabled  = false
+  tags                  = var.tags
+}
+
+resource "azurerm_private_endpoint" "acr" {
+  name                = "pe-${azurerm_container_registry.aks.name}"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  subnet_id           = azurerm_subnet.privateEndpoints.id
+  tags                = var.tags
+
+  private_service_connection {
+    name                           = "psc-${azurerm_container_registry.aks.name}"
+    private_connection_resource_id = azurerm_container_registry.aks.id
+    subresource_names              = ["registry"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "default"
+    private_dns_zone_ids = [azurerm_private_dns_zone.acr.id]
+  }
 }
 
 module "aks" {
@@ -73,6 +276,16 @@ module "aks" {
 
   managed_identities = {
     user_assigned_resource_ids = [azurerm_user_assigned_identity.aks.id]
+  }
+
+  oidc_issuer_profile = {
+    enabled = true
+  }
+
+  security_profile = {
+    workload_identity = {
+      enabled = true
+    }
   }
 
   sku = {
@@ -149,33 +362,33 @@ module "aks" {
         max_surge = "33%"
       }
     }
-    testpool = {
-      name                 = "testpool"
-      type                 = "VirtualMachines"
-      mode                 = "User"
-      orchestrator_version = "1.36.3"
-      vnet_subnet_id       = azurerm_subnet.nodes.id
-      node_labels = {
-        workload = "test"
-      }
-      upgrade_settings = {
-        max_surge = "33%"
-      }
-      virtual_machines_profile = {
-        scale = {
-          manual = [
-            {
-              size  = "Standard_D4ds_v5"
-              count = 2
-            },
-            {
-              size  = "Standard_D8ds_v5"
-              count = 1
-            }
-          ]
-        }
-      }
-    }
+    # testpool = {
+    #   name                 = "testpool"
+    #   type                 = "VirtualMachines"
+    #   mode                 = "User"
+    #   orchestrator_version = "1.36.3"
+    #   vnet_subnet_id       = azurerm_subnet.nodes.id
+    #   node_labels = {
+    #     workload = "test"
+    #   }
+    #   upgrade_settings = {
+    #     max_surge = "33%"
+    #   }
+    #   virtual_machines_profile = {
+    #     scale = {
+    #       manual = [
+    #         {
+    #           size  = "Standard_D4ds_v5"
+    #           count = 2
+    #         },
+    #         {
+    #           size  = "Standard_D8ds_v5"
+    #           count = 1
+    #         }
+    #       ]
+    #     }
+    #   }
+    # }
   }
 
   enable_rbac = true
@@ -187,6 +400,68 @@ module "aks" {
   tags = var.tags
 
   depends_on = [azurerm_role_assignment.aksNetworkContributor]
+}
+
+resource "azurerm_federated_identity_credential" "workload" {
+  name                      = "fic-workloaduser"
+  user_assigned_identity_id = azurerm_user_assigned_identity.workload.id
+  issuer                    = module.aks.oidc_issuer_profile_issuer_url
+  subject                   = "system:serviceaccount:default:workloaduser"
+  audience                  = ["api://AzureADTokenExchange"]
+}
+
+resource "azurerm_role_assignment" "aksAcrPull" {
+  scope                = azurerm_container_registry.aks.id
+  role_definition_name = "AcrPull"
+  principal_id         = module.aks.kubelet_identity.objectId
+  principal_type       = "ServicePrincipal"
+
+  lifecycle {
+    ignore_changes = [principal_id]
+  }
+}
+
+resource "azapi_resource" "testpool" {
+  type      = "Microsoft.ContainerService/managedClusters/agentPools@2026-01-02-preview"
+  name      = "testpool"
+  parent_id = module.aks.resource_id
+
+  body = {
+    properties = {
+      mode                = "User"
+      type                = "VirtualMachines"
+      orchestratorVersion = "1.36.3"
+      vnetSubnetID        = azurerm_subnet.nodes.id
+      nodeLabels = {
+        workload = "test"
+      }
+      upgradeSettings = {
+        maxSurge       = "33%"
+        maxUnavailable = "0"
+      }
+      virtualMachinesProfile = {
+        scale = {
+          autoscale = {
+            size     = "Standard_D4ds_v5"
+            minCount = 1
+            maxCount = 4
+          }
+        }
+      }
+    }
+  }
+
+  ignore_missing_property   = true
+  ignore_null_property      = true
+  schema_validation_enabled = false
+
+  response_export_values = [
+    "properties.currentOrchestratorVersion",
+    "properties.nodeImageVersion",
+    "properties.provisioningState",
+    "properties.virtualMachinesProfile.scale",
+    "type"
+  ]
 }
 
 resource "azurerm_public_ip" "bastion" {
--- outputs.tf
+++ outputs.tf
@@ -18,7 +18,32 @@ output "private_fqdn" {
   value       = module.aks.private_fqdn
 }
 
+output "oidc_issuer_url" {
+  description = "OIDC issuer URL used by Microsoft Entra Workload ID federated credentials."
+  value       = module.aks.oidc_issuer_profile_issuer_url
+}
+
+output "workload_identity_client_id" {
+  description = "Client ID used to annotate the workloaduser Kubernetes service account."
+  value       = azurerm_user_assigned_identity.workload.client_id
+}
+
+output "workload_identity_principal_id" {
+  description = "Principal ID used to grant the workload identity access to Azure resources."
+  value       = azurerm_user_assigned_identity.workload.principal_id
+}
+
 output "bastion_id" {
   description = "Resource ID of the Azure Bastion host used by az aks bastion."
   value       = azurerm_bastion_host.aks.id
+}
+
+output "acr_name" {
+  description = "Name of the Azure Container Registry."
+  value       = azurerm_container_registry.aks.name
+}
+
+output "acr_login_server" {
+  description = "Private login server for the Azure Container Registry."
+  value       = azurerm_container_registry.aks.login_server
 }
